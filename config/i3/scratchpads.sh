49839c4c7f564be390d600e394438e21f64f1e9b
Michael James Connor <michaelconnor468@gmail.com>
2026-02-11 19:54:54 -0500

    add htop and i3 configs

---
+++ config/i3/scratchpads.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+case $1 in
+    *)
+        if ! xdotool search --class $1; then
+            kitty --class $1 &
+            sleep 0.2
+        fi
+        i3-msg "[instance=$1] scratchpad show; [instance=$1] move position center"
+esac
