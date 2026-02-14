35d01cf951eeafcf752be793021ac6b499a6f63d
Michael James Connor <michaelconnor468@gmail.com>
2026-02-14 11:04:01 -0500

    Update tracked config folders to latest local edition

--- config/i3/scratchpads.sh
+++ config/i3/scratchpads.sh
@@ -2,7 +2,7 @@
 case $1 in
     *)
         if ! xdotool search --class $1; then
-            kitty --class $1 &
+            alacritty --class $1 &
             sleep 0.2
         fi
         i3-msg "[instance=$1] scratchpad show; [instance=$1] move position center"
